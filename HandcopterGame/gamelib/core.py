c3b5979fa1e62c0f008cf204f22116a937329d33
raphael.rheault@gmail.com <raphael.rheault@gmail.com>
2013-03-20 12:32:35 -0400

    ...

--- HandcopterGame/gamelib/core.py
+++ HandcopterGame/gamelib/core.py
@@ -25,7 +25,22 @@ class Core():
         pass
         
     def processEvents(self):
-        #Process
+        #initialise(); % Initialises OpenCV and camera.
+        #cvQueryFrame(camera); % Capture one frame.
+        #cvCvtColor(CV BGR2HSV); % Image RGB to HSV.
+        #cvInRange(min, max); % Only pixels within range.
+        #% Finds out external contour.
+        #cvFindContours(CV RETR EXTERNAL);
+        #if contour 6= null then
+        #% Determines CH for the external contour.
+        #cvConvexHull2(CV CLOCKWISE);
+        #for all convex hull points do
+        #% Generates all points from the detected CH.
+        #cvSeqPush(sequence, point);
+        #end for
+        #area = cvContourArea(sequence);
+        #end if
+        #return area;
         pass
     
     def collisionTesting(self):
